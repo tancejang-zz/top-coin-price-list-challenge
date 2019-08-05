958fea6d803f4a5d07bd0d2546b89e82c30dae1a
effendy.sutanto <effendy.sutanto@traveloka.com>
2019-08-06 02:57:25 +0700

    Migrate to docker secret

--- pricing-service/accessor/price_info.go
+++ pricing-service/accessor/price_info.go
@@ -1,6 +1,8 @@
 package accessor
 
 import (
+	"os"
+
 	"gopkg.in/resty.v0"
 )
 
@@ -8,7 +10,7 @@ import (
 func GetPricingInfo() (*resty.Response, error) {
 	resp, err := resty.R().
 		SetHeader("Content-Type", "application/json").
-		SetHeader("X-CoinAPI-Key", "A1269882-C37A-49A8-9413-270B3E2FDC5B").
+		SetHeader("X-CoinAPI-Key", os.Getenv("COINAPI_API_KEY")).
 		Get("https://rest.coinapi.io/v1/trades/latest")
 
 	return resp, err
