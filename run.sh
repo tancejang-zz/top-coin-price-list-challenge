6356281887f37bcbad596c0a583c20f790c9371b
effendy.sutanto <effendy.sutanto@traveloka.com>
2019-08-06 02:56:15 +0700

    Edit run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ if [ "$1" != "" ] && [ "$2" != "" ]; then
         sudo docker build -t "$directory_name" .
     fi
     if [ "$1" = "run" ]; then
-        sudo docker run -e COINAPI_API_KEY="A1269882-C37A-49A8-9413-270B3E2FDC5B" -p "$PORT":"$DOCKER_PORT" -it "$directory_name"
+        sudo docker run -p "$PORT":"$DOCKER_PORT" -it "$directory_name"
     fi
 else
     echo "Please specify whether you want to build or run."
